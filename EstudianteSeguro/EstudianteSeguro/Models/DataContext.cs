4b8d8fd1d7722d55946265bfea46898facc297b6
Raul Claros <a2019117860@estudiantes.upsa.edu.bo>
2020-09-01 19:42:24 -0400

    Con controlador Students

---
+++ EstudianteSeguro/EstudianteSeguro/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace EstudianteSeguro.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<EstudianteSeguro.Models.Student> Students { get; set; }
+    }
+}
---
+++ EstudianteSeguro/EstudianteSeguro/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace EstudianteSeguro.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        [Range(1, 9999)]
+        public int StudentID { get; set; }
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string LastName { get; set; }
+
+        [StringLength(30, ErrorMessage = "The field 0} must contain between {2} and {1} characters", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "0:dd/MM/yyyy", ApplyFormatInEditMode = false)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
